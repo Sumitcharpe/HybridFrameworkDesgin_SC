fe018a0e6715b37b32a1dec891eb1e842ea288bc
Sumitcharpe <sumit.charpe@epam.com>
2026-01-16 00:33:46 +0530

    changes for path

--- src/test/java/DesginFramework/TestComponent/Baseclass.java
+++ src/test/java/DesginFramework/TestComponent/Baseclass.java
@@ -2,6 +2,7 @@ package DesginFramework.TestComponent;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
@@ -33,7 +34,13 @@ public class Baseclass {
 		prop = new Properties();
 
 		FileInputStream file = new FileInputStream(
-				System.getProperty("user.dir") + File.separator+"Global.properties");
+			System.getProperty("user.dir")
+			+ File.separator + "src"
+			+ File.separator + "main"
+			+ File.separator + "java"
+			+ File.separator + "DesignFramework"
+			+ File.separator + "resources"
+			+ File.separator + "Global.properties");
 
 		prop.load(file);
 
@@ -54,39 +61,54 @@ public class Baseclass {
 		return driver;
 
 	}
-	
-	//read file of Json
-	
+
+	// read file of Json
 
 	public List<HashMap<String, String>> getJsonData(String filepath) throws IOException {
-		
-		
-		String jsonfile =FileUtils.readFileToString(new File (filepath), StandardCharsets.UTF_8);
+
+		String dataJsonPath = System.getProperty("user.dir") 
+		+ File.separator + "src"
+		+ File.separator + "test"
+		+ File.separator + "java"
+		+ File.separator + "DesginFramework"
+		+ File.separator + "TestData"
+		+ File.separator + "data.json";
+
+		File file = new File(dataJsonPath);
+		if (!file.exists()) {
+			throw new FileNotFoundException("File not found: " + dataJsonPath);
+		}
+		String jsonfile = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
 		ObjectMapper mapper = new ObjectMapper();
-		
-		List<HashMap<String ,String>> data=mapper.readValue(jsonfile, new TypeReference<List<HashMap<String ,String>>>(){
-			
-		});
-		
-		return data;	
-		
+
+		// String jsonfile =FileUtils.readFileToString(new File (filepath),
+		// StandardCharsets.UTF_8);
+		// ObjectMapper mapper = new ObjectMapper();
+
+		List<HashMap<String, String>> data = mapper.readValue(jsonfile,
+				new TypeReference<List<HashMap<String, String>>>() {
+
+				});
+
+		return data;
+
 	}
-	
-	public String captureScreenShot(String testCaseName , WebDriver driver) throws IOException {
-		
-		TakesScreenshot ts = (TakesScreenshot)driver;
-		
+
+	public String captureScreenShot(String testCaseName, WebDriver driver) throws IOException {
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+
 		File sourceFile = ts.getScreenshotAs(OutputType.FILE);
-		
-		File DestinationFile = new File(System.getProperty("user.dir") +"//report//" + testCaseName + ".png");
-		
+
+		File DestinationFile = new File(System.getProperty("user.dir") + "//report//" + testCaseName + ".png");
+
 		FileUtils.copyFile(sourceFile, DestinationFile);
-		return System.getProperty("user dir") +"//report//" + testCaseName + ".png";
-		
+		return System.getProperty("user dir") + "//report//" + testCaseName + ".png";
+
 	}
-	
+
 	@AfterMethod
-	
+
 	public void closeBrowser() {
 		driver.close();
 	}
